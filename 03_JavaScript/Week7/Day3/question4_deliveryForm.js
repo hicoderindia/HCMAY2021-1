6cd66fa5fe97b02379e53c07935c3079f6906f76
Dolly <hicoder.dolly@gmail.com>
2021-07-19 15:56:44 -0700

    week7_test

---
+++ 03_JavaScript/Week7/Day3/question4_deliveryForm.js
@@ -0,0 +1,22 @@
+
+
+function copyAddress(){
+
+document.getElementById("billingFirstName").value =document.getElementById("deliveryFirstName").value;
+
+
+document.getElementById("billinglastName").value =document.getElementById("deliveryLastName").value;
+
+document.getElementById("billingEmail").value =document.getElementById("deliveryEmail").value;
+
+document.getElementById("billingAddress").value =document.getElementById("deliveryAddress").value;
+
+document.getElementById("billingAddress2").value =document.getElementById("deliveryAddress2").value;
+
+document.getElementById("billingCountry").value =document.getElementById("deliveryCountry").value;
+
+document.getElementById("billingState").value =document.getElementById("deliveryState").value;
+
+document.getElementById("billingZip").value =document.getElementById("deliveryZip").value;
+
+}
